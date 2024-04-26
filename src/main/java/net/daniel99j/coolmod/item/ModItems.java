067f768d1b64c22791adcaa623b9ba7e2f7c271a
Daniel99j2 <daniel99j1235@proton.me>
2024-04-26 21:42:23 +0800

    2

--- src/main/java/net/daniel99j/coolmod/item/ModItems.java
+++ src/main/java/net/daniel99j/coolmod/item/ModItems.java
@@ -1,12 +1,21 @@
 package net.daniel99j.coolmod.item;
 
 import net.daniel99j.coolmod.CoolMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item RUBY = RegisterItem("ruby", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RUBY);
+    }
 
     private static Item RegisterItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(CoolMod.MOD_ID, name), item);
@@ -14,5 +23,7 @@ public class ModItems {
 
     public static void registerModItems() {
         CoolMod.LOGGER.info("Registering Items for" + CoolMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
     }
 }
