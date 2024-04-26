3643fec8b2d17b6d3e5e97bac6ce05553e8e05a6
Daniel99j2 <daniel99j1235@proton.me>
2024-04-26 21:12:46 +0800

    rew

---
+++ src/main/java/net/daniel99j/coolmod/CoolModClient.java
@@ -0,0 +1,10 @@
+package net.daniel99j.coolmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoolModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/daniel99j/coolmod/CoolModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.daniel99j.coolmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CoolModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
