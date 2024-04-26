baf7e7fa4cc72aa744e596027766b86eeb49599c
Daniel99j2 <daniel99j1235@proton.me>
2024-04-26 21:26:01 +0800

    1

--- src/main/java/net/daniel99j/coolmod/CoolMod.java
+++ src/main/java/net/daniel99j/coolmod/CoolMod.java
@@ -1,5 +1,6 @@
 package net.daniel99j.coolmod;
 
+import net.daniel99j.coolmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ public class CoolMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/daniel99j/coolmod/item/ModItems.java
+++ src/main/java/net/daniel99j/coolmod/item/ModItems.java
@@ -1,4 +1,9 @@
 package net.daniel99j.coolmod.item;
 
+import net.daniel99j.coolmod.CoolMod;
+
 public class ModItems {
+    public static void registerModItems() {
+        CoolMod.LOGGER.info("Registering Items for" + CoolMod.MOD_ID);
+    }
 }
