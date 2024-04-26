800dc68769f81ff664cb026395c11dc345f2d185
Daniel99j2 <daniel99j1235@proton.me>
2024-04-26 21:31:00 +0800

    2

--- src/main/java/net/daniel99j/coolmod/item/ModItems.java
+++ src/main/java/net/daniel99j/coolmod/item/ModItems.java
@@ -1,8 +1,17 @@
 package net.daniel99j.coolmod.item;
 
 import net.daniel99j.coolmod.CoolMod;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+
+    private static Item RegisterItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(CoolMod.MOD_ID, name), item);
+    }
+
     public static void registerModItems() {
         CoolMod.LOGGER.info("Registering Items for" + CoolMod.MOD_ID);
     }
