1de929b10f7ede3ebd444f3d98723342456a0d85
Daniel99j2 <daniel99j1235@proton.me>
2024-04-27 13:43:02 +0800

    Added some things

--- src/main/java/net/daniel99j/coolmod/item/ModItems.java
+++ src/main/java/net/daniel99j/coolmod/item/ModItems.java
@@ -1,9 +1,11 @@
 package net.daniel99j.coolmod.item;
 
 import net.daniel99j.coolmod.CoolMod;
+import net.fabricmc.fabric.api.item.v1.EquipmentSlotProvider;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -12,6 +14,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item COOL = RegisterItem("cool", new Item(new FabricItemSettings()));
+    public static final Item COOL_SUNGLASSES = RegisterItem("cool_sunglasses", new Item(new FabricItemSettings().equipmentSlot(stack -> EquipmentSlot.HEAD)));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(COOL);
